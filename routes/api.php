7a95f2fc9a78fe0ceab20dca77518de8784dfffa
Mahmoud Shiref <Sh1r3f@gmail.com>
2023-05-26 22:23:07 +0300

    Add RoleController to API routes - A new RoleController was added to handle role-related requests in the API.

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ApiController;
+use App\Http\Controllers\Api\RoleController;
 use App\Http\Controllers\Api\Auth\AuthController;
 
 /*
