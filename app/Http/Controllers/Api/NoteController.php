b318f446c20ab63582768cc3ae63ed9e9a851488
Mahmoud Shiref <Sh1r3f@gmail.com>
2023-05-28 22:52:04 +0300

    🚀feat(api) add NoteController, NoteRequest, and NoteResource 🛠️refactor(api) update NoteController to retrieve and return notes with pagination ⚙️chore(api) add validation rules to NoteRequest

---
+++ app/Http/Controllers/Api/NoteController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\Note;
+use Illuminate\Http\Request;
+use App\Http\Requests\NoteRequest;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\NoteResource;
+
+class NoteController extends Controller
+{
+
+    public function __construct()
+    {
+        $this->authorizeResource(Note::class, 'note');
+    }
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        $students = Note::filter($request)
+            ->search($request->q, ['name', 'points'])
+            ->order($request->options['sortBy'] ?? [])
+            ->paginate($request->options['itemsPerPage'] ?? 10, ['*'], 'page', $request->options['page'] ?? 1)
+            ->withQueryString();
+
+        return NoteResource::collection($students);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(NoteRequest $request)
+    {
+        $data = $request->validated();
+        $note = $request->user()->notes()->create($data);
+
+        return response()->json([
+            'status'  => 'success',
+            'message' => __('Note created successfully'),
+            'user'    => new NoteResource($note)
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Note $note)
+    {
+        return new NoteResource($note);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(NoteRequest $request, Note $note)
+    {
+        $data = $request->validated();
+        $note->update($data);
+
+        return response()->json([
+            'status'  => 'success',
+            'message' => __('Note updated successfully'),
+            'user'    => new NoteResource($note)
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Note $note)
+    {
+        $note->delete();
+
+        return response()->json([
+            'status'  => 'success',
+            'message' => __('Note deleted successfully')
+        ]);
+    }
+}
