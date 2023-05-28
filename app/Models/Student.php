7bc168b149c223dff4991bef2e62a84c139cdb98
Mahmoud Shiref <Sh1r3f@gmail.com>
2023-05-28 22:30:06 +0300

    ⚙️chore(student) add filter method to scope students by classroom in admin panel

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -6,7 +6,9 @@ use App\Models\Note;
 use App\Models\Classroom;
 use App\Traits\Orderable;
 use App\Traits\Searchable;
+use Illuminate\Http\Request;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Student extends Model
@@ -15,6 +17,13 @@ class Student extends Model
 
     protected $fillable = ['name', 'classroom_id'];
 
+    /**
+     * Filter students in admin panel
+     */
+    public function scopeFilter(Builder $query, Request $request)
+    {
+        $query->when($request->classroom, fn ($query, $classroom) => $query->where('classroom_id', $classroom));
+    }
 
     /**
      * Classroom it belongs to
